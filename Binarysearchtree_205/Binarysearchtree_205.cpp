358e41ca0c262a6f126395687b890583144594f9
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-07-02 12:09:30 +0700

    void preorder

--- Binarysearchtree_205/Binarysearchtree_205.cpp
+++ Binarysearchtree_205/Binarysearchtree_205.cpp
@@ -84,5 +84,18 @@ public:
 			inorder(ptr->rightchild);
 		}
 	}
+	void preorder(Node* ptr)
+	{
+		if (ROOT == NULL)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+			cout << ptr->info << " ";
+			preorder(ptr->leftchild);
+			preorder(ptr->rightchild);
+		}
+	}
 
-	

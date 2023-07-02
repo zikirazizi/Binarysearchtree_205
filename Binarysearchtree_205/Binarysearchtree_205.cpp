31f9eef037435483ad14712002c3d10ebaa593c0
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-07-02 12:00:00 +0700

    membuat if

--- Binarysearchtree_205/Binarysearchtree_205.cpp
+++ Binarysearchtree_205/Binarysearchtree_205.cpp
@@ -39,4 +39,10 @@ public:
 		Node* currentNode = NULL;
 		search(element, parent, currentNode); // Locate the node which will be the parent of the new node to be inserted
 
+		if (parent == NULL) // If the parent is NULL  (tree is empty)
+		{
+			ROOT = newNode; // Mark the new node as ROOT
+			return; // exit
+		}
+
 		

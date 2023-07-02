343ff3ae998e55fdfdf35451caae6a8d534efae6
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-07-02 11:50:36 +0700

    Isi data string

--- Binarysearchtree_205/Binarysearchtree_205.cpp
+++ Binarysearchtree_205/Binarysearchtree_205.cpp
@@ -8,4 +8,13 @@ public:
 	string info;
 	Node* leftchild;
 	Node* rightchild;
-	
+
+	//constructor for the node class
+	Node(string i, Node* l, Node* r)
+	{
+		info = i;
+		leftchild = l;
+		rightchild = r;
+	}
+};
+

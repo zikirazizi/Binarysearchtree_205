252306454875f1fabad9db1f90eb713d097f2163
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-07-02 12:10:14 +0700

    void postoreder

--- Binarysearchtree_205/Binarysearchtree_205.cpp
+++ Binarysearchtree_205/Binarysearchtree_205.cpp
@@ -99,3 +99,21 @@ public:
 		}
 	}
 
+	void postorder(Node* ptr)
+	{
+		// Performs the postorder traversal of the tree
+		if (ROOT == NULL)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+			postorder(ptr->leftchild);
+			postorder(ptr->rightchild);
+			cout << ptr->info << " ";
+		}
+	}
+};
+
+

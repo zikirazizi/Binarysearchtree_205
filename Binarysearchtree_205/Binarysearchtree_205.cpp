598250d67213e7f574840977d7ddc0a6fdb10f2e
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-07-02 12:13:07 +0700

    membuat cout

--- Binarysearchtree_205/Binarysearchtree_205.cpp
+++ Binarysearchtree_205/Binarysearchtree_205.cpp
@@ -132,4 +132,61 @@ int main()
 		cin >> ch;
 		cout << endl;
 
+		switch (ch)
+		{
+		case '1':
+		{
+			cout << "enter a word";
+			string word;
+			cin >> word;
+			obj.insert(word);
+			break;
+		}
+		case'2':
+		{
+			obj.inorder(obj.ROOT);
+			break;
+		}
+		case '3':
+		{
+			obj.preorder(obj.ROOT);
+			break;
+		}
+		case '4':
+		{
+			obj.postorder(obj.ROOT);
+			break;
+		}
+		case '5':
+			return 0;
+		default:
+		{
+			cout << "Invailid option" << endl;
+			break;
+		}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 		

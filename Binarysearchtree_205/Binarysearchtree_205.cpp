a82f6a1fbe16fb5de841151956ec8051419b305f
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-07-02 11:57:44 +0700

    Membuat void

--- Binarysearchtree_205/Binarysearchtree_205.cpp
+++ Binarysearchtree_205/Binarysearchtree_205.cpp
@@ -28,4 +28,11 @@ public:
 		ROOT = NULL; // Instializing Root to null
 	}
 
-	
+	void insert(string element) // insert a node in the binary search tree
+	{
+		Node* newNode = new Node(element, NULL, NULL); // Allocate memory for the new node
+		newNode->info = element; // Assign value to data field of the new node 
+		newNode->leftchild = NULL; // Make the left child of the new node point to NULL
+		newNode->rightchild = NULL; // Make the right child of the new node point to NULL
+
+		

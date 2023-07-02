755d32d48fddffb016bc93f657a7cae356be1d75
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-07-02 12:08:02 +0700

    isian void atau fungsi

--- Binarysearchtree_205/Binarysearchtree_205.cpp
+++ Binarysearchtree_205/Binarysearchtree_205.cpp
@@ -56,4 +56,17 @@ public:
 
 
 	void search(string element, Node*& parent, Node*& currentNode)
-	
+	{
+		//This function searches the currentNode of the specified Node as well as the current  Node its parent
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL) && (currentNode->info != element))
+		{
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
+		}
+	}
+
